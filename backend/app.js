e66ecf9b0a513ad574bfb5978538484fa0533028
RUSHI614 <rushiraval614@gmail.com>
2026-04-25 01:51:56 +0530

    add

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
